17e738d1f198a956eb66e28884eb63e2c6edf936
Mihail20052005 <108936552+Mihail20052005@users.noreply.github.com>
2023-12-12 22:39:48 +0300

    update: add filling a shape bounded points

--- prj.cw/main.cpp
+++ prj.cw/main.cpp
@@ -3,12 +3,15 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <string>
+
 
 bool buttonPressed = false;
 bool button1 = false;
 bool buttonN = false;
 bool isRightMouseButtonPressed = false;
 int draggedPointIndex = -1;
+double point_size;
 
 struct Point {
     float x, y;
@@ -34,7 +37,14 @@ std::vector<Point> new_points;
 std::vector<Point> cancel_points;
 
 void display() {
+    
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
     glBindTexture(GL_TEXTURE_2D, textureID);
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 
     glBegin(GL_QUADS);
     glTexCoord2f(0.0f, 0.0f); glVertex2f(-1.0, -1.0);
@@ -43,18 +53,21 @@ void display() {
     glTexCoord2f(0.0f, 1.0f); glVertex2f(-1.0, 1.0);
     glEnd();
 
-    glPointSize(5.0);
-    glBegin(GL_POINTS);
-    glColor3f(1.0, 0.0, 0.0);
+    glPointSize(point_size);
 
+
+    glColor3f(1.0, 1.0, 0.0);
+
+    
+    glBegin(GL_POINTS);
     for (const Point& p : points) {
         glVertex2f(p.x, p.y);
     }
     glEnd();
 
+    
     glBegin(GL_LINES);
     glColor3f(1.0, 1.0, 1.0);
-
     if (points.size() >= 2) {
         for (size_t i = 0; i < points.size() - 1; ++i) {
             glVertex2f(points[i].x, points[i].y);
@@ -68,7 +81,17 @@ void display() {
     }
     glEnd();
 
-    if (buttonN) {
+    if (!buttonN) {
+        glBegin(GL_POLYGON);
+        glColor4f(1.0, 1.0, 1.0, 0.5);
+        for (const Point& p : points) {
+            glVertex2f(p.x, p.y);
+        }
+        glEnd();
+    }
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+
+    if (buttonN || !buttonN) {
         glPointSize(5.0);
         glBegin(GL_POINTS);
         glColor3f(1.0, 0.0, 0.0);
@@ -79,23 +102,32 @@ void display() {
 
         glBegin(GL_LINES);
         glColor3f(1.0, 1.0, 1.0);
-
         if (new_points.size() >= 2) {
             for (size_t i = 0; i < new_points.size() - 1; ++i) {
                 glVertex2f(new_points[i].x, new_points[i].y);
                 glVertex2f(new_points[i + 1].x, new_points[i + 1].y);
             }
+            glVertex2f(new_points.back().x, new_points.back().y);
+            glVertex2f(new_points.front().x, new_points.front().y);
         }
-        if ((buttonPressed || button1) && new_points.size() != 0) {
-            button1 = true;
-            glVertex2f(new_points[new_points.size() - 1].x, new_points[new_points.size() - 1].y);
-            glVertex2f(new_points[0].x, new_points[0].y);
+        glEnd();
+
+        glBegin(GL_POLYGON);
+        glColor4f(1.0, 1.0, 1.0, 0.5); 
+        for (const Point& p : new_points) {
+            glVertex2f(p.x, p.y);
         }
         glEnd();
     }
 
+    glDisable(GL_BLEND);
     glutSwapBuffers();
 }
+
+
+
+
+
 
 void reshape(int width, int height) {
     glViewport(0, 0, width, height);
@@ -236,6 +268,15 @@ void mouseMotion(int x, int y) {
 }
 
 int main(int argc, char** argv) {
+    std::string colors;
+    std::cin >> point_size;
+    std::cin >> colors;
+    GLfloat color_r = 1.0f, color_g = 1.0f, color_b = 0.0f;
+
+    color_r = float(colors[0]);
+    color_g = float(colors[1]);
+    color_b = float(colors[2]);
+
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
     glutCreateWindow("Image Display");
