be8f94e730988ebee98fe7cb509cb407992afd19
Mihail20052005 <108936552+Mihail20052005@users.noreply.github.com>
2024-01-11 18:11:09 +0300

    fix problems

--- prj.codeforces/1038a.cpp
+++ prj.codeforces/1038a.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
-using namespace std;
-string a;
+std::string result;
 int n, k, i, b[30], s = 1e8;
 int main() {
-    cin >> n >> k >> a;
+    std::cin >> n >> k >> result;
     for (i = 0; i < n; i++)
-        b[a[i] - 'A']++;
+        b[result[i] - 'A']++;
     for (i = 0; i < k; i++)
-        s = min(s, b[i]);
-    cout << s * k;
+        s = std::min(s, b[i]);
+    std::cout << s * k;
     return 0;
 }
